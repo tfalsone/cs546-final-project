6e55d2ea1079b4e52b0e2b5c4f6c0cdd4c6464da
Scott Russell <scott.tkdmaster@mac.com>
2018-05-09 15:26:54 -0400

    Scott initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+const static = express.static(__dirname + '/public');
+
+app.use(static);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/login.html'));
+})
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:3000");
+});
