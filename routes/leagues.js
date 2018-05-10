0e3e12beb5843ecf5a5ee28f74bc4e5abf03d4f0
Michael Janvier <mjanvier@stevens.edu>
2018-05-10 17:04:14 -0400

    league delete and findOne added

--- routes/leagues.js
+++ routes/leagues.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const league = require('../controllers/league.js');
+const league = require('../controllers/leagues.js');
 // get, post, put, other functions
 
 
 //get all leagues
-/*
 router.get("/", league.findAll);
 
 router.post("/", league.createLeague);
-*/
+
+router.get("/:leagueId", league.findOne);
+
+router.delete("/:leagueId", league.delete);
 module.exports = router;
