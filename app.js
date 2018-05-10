ba242802491352feec2354126f8e545019dd2baf
Michael Janvier <mjanvier@stevens.edu>
2018-05-10 10:49:18 -0400

    league model added, working on routes/data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+const bodyParser= require('body-parser')
 const static = express.static(__dirname + '/public');
 
 app.use(static);
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/public/pages/login.html'));
---
+++ models/leagues.js
@@ -0,0 +1,21 @@
+const dbConnection = require("../config/mongoConnection");
+const mongoose = require('mongoose')
+const data = require("../data/");
+const leagues = data.leagues;
+var Schema = mongoose.Schema
+
+const leagueSchema = Schema({
+    id: String,
+    name: String,
+    sport: String,
+    teams: [String],
+    games: [{
+        gameID: String,
+        teams: {team1: String, team2: String},
+        time: String,
+        location: String,
+        score: {team1: int, team2: int}
+    }]
+});
+
+module.exports = mongoose.model('League', leagueSchema);
