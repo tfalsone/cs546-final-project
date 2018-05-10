a019b64f6e8d2cb306114ff0f6949cca12b77911
Scott Russell <scott.tkdmaster@mac.com>
2018-05-10 14:20:20 -0400

    Fixed some personal merge conflicts

--- app.js
+++ app.js
@@ -8,17 +8,8 @@ app.use(static);
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname + '/pages/login.html'));
+    res.sendFile(path.join(__dirname + '/public/pages/home.html'));
 });
-
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname + '/pages/home.html'));
-});
-=======
-    res.sendFile(path.join(__dirname + '/public/pages/login.html'));
-})
->>>>>>> 64c2cfc40530cf80055bac3f7af57f2c52bbd015
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
