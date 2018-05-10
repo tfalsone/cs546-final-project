81c606e3e735fee2450522000148c742306159fa
Scott Russell <scott.tkdmaster@mac.com>
2018-05-10 14:14:25 -0400

    Scott changes from yesterday

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.use(static);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + '/pages/login.html'));
-})
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/home.html'));
+});
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
